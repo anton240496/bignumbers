5895a102dfc3a720adca1e878ab79f98c0953562
anton <anton240496@mail.ru>
2020-05-06 11:51:48 +0300

    тесты

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,36 @@
 package com.company;
-
 import java.math.BigInteger;
-
+import org.junit.Test;
 public class Main {
-
-
-    // write your code here
+    @Test
     public static void main(String[] args) {
+
         BigInteger a = new BigInteger ("9000000000000000000");
         BigInteger b = new BigInteger ("8444444444444444444");
-       // BigInteger[] array = {9000000000000000000,8444444444444444444};
 
-
-        BigInteger sumarg;
-
-
-        BigInteger sum = a.add(b);
-        sumarg = sum.divide(BigInteger.valueOf(2));
-
-
-
-        System.out.println(sumarg);
+        System.out.println(arg(a,b));
 
     }
 
+    @Test
+    public static void main1(String[] args) {
+
+        BigInteger a = new BigInteger ("9000000000000000000");
+        BigInteger b = new BigInteger ("8444444444444444444");
+
+        System.out.println(arg(a,b));
+
+    }
+
+    private static BigInteger arg(BigInteger a, BigInteger b){
+//        a = new BigInteger ("9000000000000000000");
+//        b = new BigInteger ("8444444444444444444");
+
+        BigInteger sum = a.add(b);
+        BigInteger sumarg = sum.divide(BigInteger.valueOf(2));
+
+        return sumarg;
+
+
+    }
 }
